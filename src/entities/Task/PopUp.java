e225c6fbb2e4d6e8a0ba2ca1c403d6b50648aa2d
eldariko <bizybone15@gmail.com>
2015-12-17 22:08:39 +0200

    g

--- src/entities/Task/PopUp.java
+++ src/entities/Task/PopUp.java
@@ -1,15 +1,51 @@
 package entities.Task;
 
+import java.io.Serializable;
 import java.sql.Date;
 
-import entities.person.Person;
 import entities.sms.SMS;
 
-public class PopUp {
+public class PopUp implements Serializable {
 	
-	PopUpTemplates template;
-	String text;
-	boolean triggerSms;
-	Date DateTimeSend;
-	SMS sms;
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private PopUpTemplates template;
+	private String text;
+	private boolean triggerSms;
+	private Date DateTimeSend;
+	private SMS sms;
+	
+	
+	public PopUpTemplates getTemplate() {
+		return template;
+	}
+	public void setTemplate(PopUpTemplates template) {
+		this.template = template;
+	}
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+	public boolean isTriggerSms() {
+		return triggerSms;
+	}
+	public void setTriggerSms(boolean triggerSms) {
+		this.triggerSms = triggerSms;
+	}
+	public Date getDateTimeSend() {
+		return DateTimeSend;
+	}
+	public void setDateTimeSend(Date dateTimeSend) {
+		DateTimeSend = dateTimeSend;
+	}
+	public SMS getSms() {
+		return sms;
+	}
+	public void setSms(SMS sms) {
+		this.sms = sms;
+	}
 }
--- src/entities/person/Person.java
+++ src/entities/person/Person.java
@@ -1,5 +1,6 @@
 package entities.person;
 
+import java.io.Serializable;
 import java.math.BigInteger;
 import java.sql.Date;
 import java.util.ArrayList;
@@ -9,13 +10,66 @@ import entities.GPS.Gps;
 import entities.Task.PopUp;
 import entities.Task.TasksBank;
 
-public class Person {
+public class Person implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private String fullName;
 	private BigInteger personId;
-	String mail;
-	int age;
-	Date DateTimeRegister;
-	TasksBank tasks;
-	Gps gps;
-	DefaultSettings defaultSettings;   
+	private String mail;
+	private int age;
+	private Date DateTimeRegister;
+	private TasksBank tasks;
+	private Gps gps;
+	private DefaultSettings defaultSettings;
+	public String getFullName() {
+		return fullName;
+	}
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+	public BigInteger getPersonId() {
+		return personId;
+	}
+	public void setPersonId(BigInteger personId) {
+		this.personId = personId;
+	}
+	public String getMail() {
+		return mail;
+	}
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public Date getDateTimeRegister() {
+		return DateTimeRegister;
+	}
+	public void setDateTimeRegister(Date dateTimeRegister) {
+		DateTimeRegister = dateTimeRegister;
+	}
+	public TasksBank getTasks() {
+		return tasks;
+	}
+	public void setTasks(TasksBank tasks) {
+		this.tasks = tasks;
+	}
+	public Gps getGps() {
+		return gps;
+	}
+	public void setGps(Gps gps) {
+		this.gps = gps;
+	}
+	public DefaultSettings getDefaultSettings() {
+		return defaultSettings;
+	}
+	public void setDefaultSettings(DefaultSettings defaultSettings) {
+		this.defaultSettings = defaultSettings;
+	}
+	  
 }
